20b979331f74eece15f29ca6945a0ae67efa15ec
Hritik <hritikmokase@gmail.com>
2026-02-25 00:00:10 +0530

    Add Port

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -203,6 +203,7 @@ app.delete("/api/v1/content", Auth, async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
